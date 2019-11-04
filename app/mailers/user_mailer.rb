eeb9917787c462c9e1690fa5ce7659aac1c2f7f0
Antoinebousquet <moodlinkproject@gmail.com>
2019-11-04 18:25:08 +0100

    sixth

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://eventbriteproject.herokuapp.com/' 
+    @url  = 'https://eventbriteproject.herokuapp.com' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
