93fabf2771b0f8453d7027104116e46d11fc87c6
mariannehbt <marianne.heribert@gmail.com>
2019-11-05 21:58:43 +0100

    test sur sign in no.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      notice: 'User was successfully created.'
+      redirect_to event_path, notice: 'User was successfully created.'
     else
       flash[:danger] = 'Something went wrong - please check your input'
       render :new
